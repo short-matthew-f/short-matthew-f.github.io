28d431785d1da980c64e622d623c4f05cea9c832
Matthew Short <short.matthew.f@gmail.com>
2026-08-21 11:05:24 -0400

    Add Lane Warden M0 v0.1.3 service worker

---
+++ lol-the-spire/lane-warden-m0-v0.1.3/sw.js
@@ -0,0 +1,5 @@
+const CACHE='lane-warden-m0-0.1.3';
+const ASSETS=['./','./index.html','./instrumentation.js','./manifest.webmanifest','../lane-warden-m0-v0.1.2/styles.css','../lane-warden-m0-v0.1.1/main.js','../lane-warden-m0-v0.1.1/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;})))});
