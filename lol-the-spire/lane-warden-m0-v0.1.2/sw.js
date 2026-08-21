bbbb4c7b0c73128152e389a054ccf6441e443c45
Matthew Short <short.matthew.f@gmail.com>
2026-08-21 10:13:41 -0400

    Cache immutable v0.1.1 core plus v0.1.2 instrumentation

--- lol-the-spire/lane-warden-m0-v0.1.2/sw.js
+++ lol-the-spire/lane-warden-m0-v0.1.2/sw.js
@@ -1,5 +1,5 @@
 const CACHE='lane-warden-m0-0.1.2';
-const ASSETS=['./','./index.html','./styles.css','./main.js','./manifest.webmanifest','./icon.svg'];
+const ASSETS=['./','./index.html','./styles.css','./instrumentation.js','../lane-warden-m0-v0.1.1/main.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
