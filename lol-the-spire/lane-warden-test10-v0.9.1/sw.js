179d408a938ca15df4ec354e4dc5d764566db86b
Matthew Short <short.matthew.f@gmail.com>
2026-08-22 19:18:16 -0400

    Add Test 10 v0.9.1 service worker

---
+++ lol-the-spire/lane-warden-test10-v0.9.1/sw.js
@@ -0,0 +1,5 @@
+const CACHE='lane-warden-test10-0.9.1-v1';
+const ASSETS=['./','./index.html','./test10.js','./manifest.webmanifest','./LW-T10-002-DECLARATION.md','./README.md','./VERSION','../lane-warden-test10-v0.9.0/index.html','../lane-warden-test10-v0.9.0/test10.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('lane-warden-test10-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request)));});
